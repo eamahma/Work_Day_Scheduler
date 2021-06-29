436bf3b2be727feae0057b20a725925490f918e1
eamahma <eamahma@gmail.com>
2021-06-29 08:24:11 -0400

    added comments

--- js/script.js
+++ js/script.js
@@ -2,34 +2,48 @@
 var weekDay = moment().format("ddd MMM Do, YYYY");
 $('#currentDay').text(weekDay);
 
+// When document loaded ...
 $(document).ready(function(){
+    // when saveBtn pressed
     $(".saveBtn").on("click",function(){
+        // get sibling to saveBtn -> .description (textbox) value
         var textValue = $(this).siblings(".description").val();
+        // get parent attribute id (hour)
         var time = $(this).parent().attr("id");
 
+        // store time and value into local storage
         localStorage.setItem(time, textValue);
     })
     
+    // call function to update time blocks color
     function timeBlockUpdate() {
+        // get the current hour
         var currentHour = moment().hours();
+        // for each timeblock ...
         $(".time-block").each(function(){
+            // get row element id and convert to int
             var blockHour = parseInt($(this).attr("id"));
 
+            // if time block hour is in past
             if (blockHour < currentHour) {
                 $(this).addClass("past");
+            // if time block is present
             } else if (blockHour === currentHour) {
                 $(this).removeClass("past");
-                $(this).addClass("present");  
+                $(this).addClass("present");
+            // otherwise, is in future
             } else {
                 $(this).removeClass("past");
-                $(this).removeClass("ppresent");
+                $(this).removeClass("present");
                 $(this).addClass("future");                 
             }
         })
     }
 
+    // call function to update time blocks color
     timeBlockUpdate();
 
+    // get local storage value for each time block
     $("#9 .description").val(localStorage.getItem("9"));
     $("#10 .description").val(localStorage.getItem("10"));
     $("#11 .description").val(localStorage.getItem("11"));
